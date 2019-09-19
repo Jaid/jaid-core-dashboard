05682fe77992d1d25e07f68f5a6f1e3ad9bd3924
Jaid <jaid.jsx@gmail.com>
2019-09-20 01:50:41 +0200

    Initialized based on https://github.com/Jaid/epoch-seconds

---
+++ readme.md
@@ -0,0 +1,3 @@
+# jaid-core-dashboard
+## Under construction
+Not much to see right now. Come back lator! :crocodile:
---
+++ src/index.js
@@ -0,0 +1,23 @@
+/** @module jaid-core-dashboard */
+
+/**
+ * @function
+ * @param {number} [compareValue]
+ * @returns {number} Seconds passed since Unix epoch (01 January 1970)
+ * @example
+ * import jaidCoreDashboard from "jaid-core-dashboard"
+ * const result = jaidCoreDashboard()
+ * result === 1549410770
+ * setTimeout(() => {
+ *   const result2 = jaidCoreDashboard(result)
+ *   result2 === 3
+ * }, 3000)
+ */
+export default compareValue => {
+  const seconds = Math.floor(Date.now() / 1000)
+  if (compareValue === undefined) {
+    return seconds
+  } else {
+    return seconds - compareValue
+  }
+}
