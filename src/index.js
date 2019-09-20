2d5ebd5fe93b5195b653c5ce6d18c7035dbc8b82
Jaid <jaid.jsx@gmail.com>
2019-09-20 04:07:08 +0200

    Added some stubs
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
 /** @module jaid-core-dashboard */
 
-/**
- * @function
- * @param {number} [compareValue]
- * @returns {number} Seconds passed since Unix epoch (01 January 1970)
- * @example
- * import jaidCoreDashboard from "jaid-core-dashboard"
- * const result = jaidCoreDashboard()
- * result === 1549410770
- * setTimeout(() => {
- *   const result2 = jaidCoreDashboard(result)
- *   result2 === 3
- * }, 3000)
- */
-export default compareValue => {
-  const seconds = Math.floor(Date.now() / 1000)
-  if (compareValue === undefined) {
-    return seconds
-  } else {
-    return seconds - compareValue
+import {isEmpty} from "has-content"
+import router from "fast-koa-router"
+
+export default class {
+
+  constructor() {}
+
+  getConfigSetup = {
+    secretKeys: ["dashboardPassword"],
   }
+
+  setCoreReference(core) {
+    /**
+     * @type {import("koa")}
+     */
+    this.koa = core.koa
+  }
+
+  handleConfig(config) {
+    if (isEmpty(config.dashboardPassword)) {
+      return false
+    }
+    this.dashboardPassword = config.dashboardPassword
+  }
+
+  init() {
+    const routes = {
+      get: {
+        "/status": "",
+      },
+    }
+    this.koa.use(router(routes))
+  }
+
 }
--- test/test.js
+++ test/test.js
@@ -1,13 +1,26 @@
 import path from "path"
 
+import JaidCore from "jaid-core"
+import getPort from "get-port"
+
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
 
 /**
  * @type { import("../src") }
  */
-const {default: jaidCoreDashboard} = indexModule
+const {default: JaidCoreDashboard} = indexModule
 
-it("should run", () => {
-  const result = jaidCoreDashboard()
-  expect(result).toBeGreaterThan(1549410770)
+it("should run", async () => {
+  const insecurePort = await getPort()
+  const dashboardPlugin = new JaidCoreDashboard()
+  const core = new JaidCore({
+    insecurePort,
+    name: _PKG_TITLE,
+    folder: ["Jaid", _PKG_TITLE, "test", new Date().toISOString()],
+    version: _PKG_VERSION,
+  })
+  await core.init({
+    dashboard: dashboardPlugin,
+  })
+  await core.close()
 })
